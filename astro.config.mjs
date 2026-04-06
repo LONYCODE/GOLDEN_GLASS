c84234164224b9972c1263aafcdb1922b73bfbc5
LONYCODE <muhammadalhamwi963@gmail.com>
2026-04-06 04:03:05 +0300

    Fix: match base path with repository name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+// astro.config.mjs
 export default defineConfig({
-  site: 'https://LONYCODE.github.io', 
-  base: '/goldenglass', 
+  site: 'https://LONYCODE.github.io',
+  base: '/GOLDEN_GLASS', // تأكد أنها تطابق اسم الريبو حرفياً
   integrations: [tailwind()],
 });
